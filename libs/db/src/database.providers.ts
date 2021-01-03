4bfbb7ea8b5f03ec353604f6950b82831f9104a6
pipisasa <emir0725mk@gmail.com>
2021-01-03 21:45:22 +0600

    build app

--- libs/db/src/database.providers.ts
+++ libs/db/src/database.providers.ts
@@ -7,7 +7,8 @@ export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
     useFactory: async () => {
-      const dbConfig = parseDbUrl('postgres://sizufkfijeumaz:a3f6da68daaaa8cc5109bfbd423ff21e7373ccab565fc38e62f1e64dafce56f2@ec2-3-215-40-176.compute-1.amazonaws.com:5432/ddfa3qgp8gjdgt');
+      // const dbConfig = parseDbUrl('postgres://sizufkfijeumaz:a3f6da68daaaa8cc5109bfbd423ff21e7373ccab565fc38e62f1e64dafce56f2@ec2-3-215-40-176.compute-1.amazonaws.com:5432/ddfa3qgp8gjdgt');
+      const dbConfig = parseDbUrl(process.env.DATABASE_URL);
       console.log("DATABASE_URI", process.env.DATABASE_URL, dbConfig);
 
       // const sequelize = new Sequelize(process.env.DATABASE_URL || configuration().db.postgres.uri, {
