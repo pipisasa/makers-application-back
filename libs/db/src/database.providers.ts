4b45e2afeee089d733fd8cc461aa73fcb8755bd9
pipisasa <emir0725mk@gmail.com>
2021-01-03 21:41:49 +0600

    build app

--- libs/db/src/database.providers.ts
+++ libs/db/src/database.providers.ts
@@ -1,14 +1,22 @@
 import { Sequelize } from 'sequelize-typescript';
 import configuration from 'libs/config/src/index';
 import { User } from './user/user.entity';
+import * as parseDbUrl from 'parse-database-url';
 
 export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
     useFactory: async () => {
-      console.log("DATABASE_URI", process.env.DATABASE_URL);
+      const dbConfig = parseDbUrl('postgres://sizufkfijeumaz:a3f6da68daaaa8cc5109bfbd423ff21e7373ccab565fc38e62f1e64dafce56f2@ec2-3-215-40-176.compute-1.amazonaws.com:5432/ddfa3qgp8gjdgt');
+      console.log("DATABASE_URI", process.env.DATABASE_URL, dbConfig);
 
-      const sequelize = new Sequelize(process.env.DATABASE_URL || configuration().db.postgres.uri, {
+      // const sequelize = new Sequelize(process.env.DATABASE_URL || configuration().db.postgres.uri, {
+      //   logging: false,
+      // });
+      const sequelize = new Sequelize(dbConfig.database,dbConfig.user,dbConfig.password, {
+        host: dbConfig.host,
+        port: dbConfig.port,
+        dialect: dbConfig.driver,
         logging: false,
       });
       sequelize.addModels([User]);
