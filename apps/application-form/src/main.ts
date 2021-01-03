3f3918bda967cb0bfb89ed4c4c0345efc7f0ec59
pipisasa <emir0725mk@gmail.com>
2021-01-03 14:32:02 +0600

    build app

--- apps/application-form/src/main.ts
+++ apps/application-form/src/main.ts
@@ -1,19 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {readFileSync} from 'fs';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 // import * as express from 'express';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: readFileSync('key.pem'),
-    cert: readFileSync('cert.pem'),
-  };
+  // const httpsOptions = {
+  //   key: readFileSync(join('key.pem')),
+  //   cert: readFileSync(join('cert.pem')),
+  // };
 
   const app = await NestFactory.create(AppModule, {
     bodyParser: true,
-    httpsOptions,
+    // httpsOptions,
   });
 
   app.useGlobalPipes(
--- dist/apps/application-form/apps/application-form/src/main.js
+++ dist/apps/application-form/apps/application-form/src/main.js
@@ -11,17 +11,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const common_1 = require("@nestjs/common");
 const core_1 = require("@nestjs/core");
-const fs_1 = require("fs");
 const app_module_1 = require("./app.module");
 function bootstrap() {
     return __awaiter(this, void 0, void 0, function* () {
-        const httpsOptions = {
-            key: fs_1.readFileSync('key.pem'),
-            cert: fs_1.readFileSync('cert.pem'),
-        };
         const app = yield core_1.NestFactory.create(app_module_1.AppModule, {
             bodyParser: true,
-            httpsOptions,
         });
         app.useGlobalPipes(new common_1.ValidationPipe({
             transform: true,
