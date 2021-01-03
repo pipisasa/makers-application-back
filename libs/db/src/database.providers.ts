e07afc4c730740b61cd4de3ed3cfa30283fb103e
pipisasa <emir0725mk@gmail.com>
2021-01-03 22:09:23 +0600

    build app

--- libs/db/src/database.providers.ts
+++ libs/db/src/database.providers.ts
@@ -1,25 +1,29 @@
 import { Sequelize } from 'sequelize-typescript';
 import configuration from 'libs/config/src/index';
 import { User } from './user/user.entity';
-import * as parseDbUrl from 'parse-database-url';
+// import * as parseDbUrl from 'parse-database-url';
 
 export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
     useFactory: async () => {
-      const dbConfig = parseDbUrl('postgres://sizufkfijeumaz:a3f6da68daaaa8cc5109bfbd423ff21e7373ccab565fc38e62f1e64dafce56f2@ec2-3-215-40-176.compute-1.amazonaws.com:5432/ddfa3qgp8gjdgt');
+      // const dbConfig = parseDbUrl(configuration().db.postgres.uri);
       // const dbConfig = parseDbUrl(process.env.DATABASE_URL);
-      console.log("DATABASE_URI", process.env.DATABASE_URL, dbConfig);
+      console.log("DATABASE_URI", configuration().db.postgres.heroku);
 
-      // const sequelize = new Sequelize(process.env.DATABASE_URL || configuration().db.postgres.uri, {
+      const sequelize = new Sequelize(configuration().db.postgres.heroku, {
+        logging: false,
+        ssl: false,
+        dialect:  'postgres',
+        protocol: 'postgres',
+      });
+      // const sequelize = new Sequelize(dbConfig.database,dbConfig.user,dbConfig.password, {
+      //   ssl: false,
+      //   host: dbConfig.host,
+      //   port: dbConfig.port,
+      //   dialect: dbConfig.driver,
       //   logging: false,
       // });
-      const sequelize = new Sequelize(dbConfig.database,dbConfig.user,dbConfig.password, {
-        host: dbConfig.host,
-        port: dbConfig.port,
-        dialect: dbConfig.driver,
-        logging: false,
-      });
       sequelize.addModels([User]);
       await sequelize.sync({ force: !true });
       return sequelize;
