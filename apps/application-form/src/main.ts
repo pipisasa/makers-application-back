1ca2a48496f2cba6fb88fffb1ba54595498b1301
pipisasa <emir0725mk@gmail.com>
2021-01-03 14:21:37 +0600

    add ssl certificate

--- apps/application-form/src/main.ts
+++ apps/application-form/src/main.ts
@@ -1,12 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import fs from 'fs';
 import { AppModule } from './app.module';
 
 // import * as express from 'express';
 
 async function bootstrap() {
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/private-key.pem'),
+    cert: fs.readFileSync('./secrets/public-certificate.pem'),
+  };
+
   const app = await NestFactory.create(AppModule, {
     bodyParser: true,
+    httpsOptions,
   });
 
   app.useGlobalPipes(
