074cb7d9a00d9f8f2207def50c775fb0cc6006b5
pipisasa <emir0725mk@gmail.com>
2021-01-03 22:09:49 +0600

    build app

--- libs/db/src/database.providers.ts
+++ libs/db/src/database.providers.ts
@@ -1,13 +1,13 @@
 import { Sequelize } from 'sequelize-typescript';
 import configuration from 'libs/config/src/index';
 import { User } from './user/user.entity';
-// import * as parseDbUrl from 'parse-database-url';
+import * as parseDbUrl from 'parse-database-url';
 
 export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
     useFactory: async () => {
-      // const dbConfig = parseDbUrl(configuration().db.postgres.uri);
+      const dbConfig = parseDbUrl(configuration().db.postgres.heroku);
       // const dbConfig = parseDbUrl(process.env.DATABASE_URL);
       console.log("DATABASE_URI", configuration().db.postgres.heroku);
 
@@ -16,6 +16,8 @@ export const databaseProviders = [
         ssl: false,
         dialect:  'postgres',
         protocol: 'postgres',
+        port: dbConfig.port,
+        host: dbConfig.host,
       });
       // const sequelize = new Sequelize(dbConfig.database,dbConfig.user,dbConfig.password, {
       //   ssl: false,
