28e4246a232dd60f726788103d8da57a8e9df778
pipisasa <emir0725mk@gmail.com>
2021-01-03 14:26:39 +0600

    build app

--- apps/application-form/src/main.ts
+++ apps/application-form/src/main.ts
@@ -1,14 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import fs from 'fs';
+import {readFileSync} from 'fs';
 import { AppModule } from './app.module';
 
 // import * as express from 'express';
 
 async function bootstrap() {
   const httpsOptions = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem'),
+    key: readFileSync('key.pem'),
+    cert: readFileSync('cert.pem'),
   };
 
   const app = await NestFactory.create(AppModule, {
